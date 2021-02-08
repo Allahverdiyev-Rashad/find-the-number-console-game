7ccf4c5bf53333fbcf88ba04568f89f75331f736
Allahverdiyev-Rashad <developer.rashad.allahverdiyev@gmail.com>
2021-02-08 18:03:04 +0400

    Adding Color class and some changes Main class

--- src/Main.java
+++ src/Main.java
@@ -9,22 +9,22 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int number = rand.nextInt(100);
         boolean status = false,
-        err = false;
+                err = false;
         int[] guesses = new int[5];
 
-        System.out.println("Welcome to game !\n" +
-                "I kept a number between 0 and 99");
+        System.out.println(Color.GREEN + "Welcome to the game !\n" + Color.BLUE +
+                "I kept a number between 0 and 99" + Color.RESET);
         while (health > 0) {
-            System.out.println("Your guess : ");
+            System.out.print(Color.PURPLE + "Your guess : " + Color.RESET);
             guess = scan.nextInt();
             if ((guess < 0) || (guess > 99)) {
                 if (err) {
-                    System.out.println("You entered 2 times unacceptable number and You lost 1 health\n" +
-                            "Current health : " + --health);
+                    System.out.println(Color.RED + "You entered 2 times unacceptable number and You lost 1 health\n" +
+                            "Current health : " + Color.RESET + --health);
                 } else {
                     err = true;
                 }
-                System.out.println("Please enter a number between 0 and 99 :");
+                System.out.println(Color.RED + "Please enter a number between 0 and 99 : " + Color.RESET);
                 continue;
             }
             guesses[i++] = guess;
@@ -32,22 +32,21 @@ public class Main {
                 status = true;
                 break;
             } else {
-                System.out.println("False, please try again ! Health : " + --health);
+                System.out.println(Color.CYAN + "False, please try again ! Health : " + Color.RESET + --health);
             }
         }
         if (status) {
-            System.out.println("Congratulations! Your guess is true\n" +
+            System.out.println(Color.GREEN + "Congratulations!" + Color.BLUE + "Your guess is true\n" +
                     "The number is : " + number +
-                    "\nHealth : " + health);
+                    "\nHealth : " + Color.RESET + health);
         } else {
-            System.out.println("You failed !");
+            System.out.println(Color.RED + "You failed !" + Color.RESET);
         }
-        System.out.println("Your guesses : ");
+        System.out.print(Color.PURPLE + "Your guesses : " + Color.RESET);
         for (int value :
                 guesses) {
             if (value != 0)
-                System.out.print(value + "-");
+                System.out.print(Color.YELLOW + value + "-" + Color.RESET);
         }
-
     }
 }
